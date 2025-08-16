f5424d099460ed5d3314f04c3ac4e3e8be19da3b
ritesh-chauhan0x1 <22054368@kiit.ac.in>
2025-08-17 04:31:26 +0530

    Initial commit: Quiz-Platform

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to "Quiz-Platform"</h1>
+        <p>"Educational tool with analytics"</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
